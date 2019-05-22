5ec60f947ce7ab15b9606f33ef27893badbd5698
Alain GONZE <alain.gonze@production.infra>
2019-05-22 14:21:32 +0200

    parametrage webpack + mise en place template homepage 2

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+import '../css/app.scss';
+// loads the jquery package from node_modules
+var $ = require('jquery');
+
+require('bootstrap');
+
+/*$(document).ready(function() {
+ alert('test');
+});*/
