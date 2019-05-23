215097d29c2e7afdf740b678b2f3357572f5011b
alain.gonze <armelgonze@yahoo.fr>
2019-05-23 15:30:29 +0200

    Installation et parametrage FOSUSER+ HOver et animate.css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
-import '../css/app.scss';
 // loads the jquery package from node_modules
+import '../css/app.scss';
 var $ = require('jquery');
 
 require('bootstrap');
+require('animate.css');
+require('hover.css');
 
 /*$(document).ready(function() {
  alert('test');
